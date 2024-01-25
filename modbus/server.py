1decd3eed090de66795bd0844f42ae39aaddc2a0
samson0v <vitalikbidochka@gmail.com>
2024-01-25 10:44:43 +0200

    Updated Modbus demo server

--- modbus/server.py
+++ modbus/server.py
@@ -23,16 +23,16 @@ def run_server():
     builder = BinaryPayloadBuilder(byteorder=Endian.LITTLE,
                                    wordorder=Endian.LITTLE)
     builder.add_string('abcd')
-    builder.add_bits([False, True, False, True, True, False, True, False])
+    builder.add_bits(
+        [False, True, False, True, True, False, True, True, True, True, False, True, False, False, True, False])
     builder.add_8bit_int(-0x12)
     builder.add_8bit_uint(0x12)
     builder.add_16bit_int(-0x5678)
     builder.add_16bit_uint(0x1234)
     builder.add_32bit_int(-0x1234)
     builder.add_32bit_uint(0x12345678)
-    builder.add_16bit_float(12.34)
-    builder.add_16bit_float(-12.34)
-    builder.add_32bit_float(22.34)
+    builder.add_16bit_float(12.34375)
+    builder.add_32bit_float(223546.34375)
     builder.add_32bit_float(-22.34)
     builder.add_64bit_int(-0xDEADBEEF)
     builder.add_64bit_uint(0x12345678DEADBEEF)
@@ -40,7 +40,12 @@ def run_server():
     builder.add_64bit_float(123.45)
     builder.add_64bit_float(-123.45)
     block = ModbusSequentialDataBlock(1, builder.to_registers())
-    store = ModbusSlaveContext(di=block, co=block, hr=block, ir=block)
+    builder_for_coils = BinaryPayloadBuilder(byteorder=Endian.LITTLE,
+                                             wordorder=Endian.LITTLE)
+    builder_for_coils.add_bits(
+        [False, True, False, True, True, False, True, True, True, True, False, True, False, False, True, False])
+    coils_block = ModbusSequentialDataBlock(1, builder_for_coils.to_coils())
+    store = ModbusSlaveContext(di=coils_block, co=coils_block, hr=block, ir=block)
     slaves = {
         0x01: store
     }
